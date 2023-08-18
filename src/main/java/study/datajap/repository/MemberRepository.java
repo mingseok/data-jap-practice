1737949bd544a03a9357b42f2906aefef44701f8
minseok <alstjr706@naver.com>
2023-08-18 23:36:04 +0900

    feat : (spring data jpa) MemberRepository @Query 사용한 코드 구현

--- src/main/java/study/datajap/repository/MemberRepository.java
+++ src/main/java/study/datajap/repository/MemberRepository.java
@@ -2,7 +2,27 @@ package study.datajap.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajap.dto.MemberDto;
 import study.datajap.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findTop3HelloBy();
+
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.datajap.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
