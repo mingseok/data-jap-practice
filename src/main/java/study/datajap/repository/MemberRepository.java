e61fd01e063bd1f39c41bd4216a97b1e135c64c3
minseok <alstjr706@naver.com>
2023-08-19 23:55:43 +0900

    feat : (spring data jpa) Member리포지토리 여러가지의 반환타입이 가능한 케이스

--- src/main/java/study/datajap/repository/MemberRepository.java
+++ src/main/java/study/datajap/repository/MemberRepository.java
@@ -8,6 +8,7 @@ import study.datajap.dto.MemberDto;
 import study.datajap.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -28,4 +29,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String username); // 컬렉션
+    Member findMemberByUsername(String username); // 단건
+    Optional<Member> findOptionalByUsername(String username); // 단건 Optional
+
+
 }
